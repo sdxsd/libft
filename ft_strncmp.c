52e1447624dd89c701e47e734f0b39a26d8f66ea
sdxsd <willmaguire@icloud.com>
2021-10-11 15:08:31 +0200

    h

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,13 +5,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	iterator;
 
 	iterator = 0;
-	while (((unsigned char) s1[iterator] != '\0' \
-	&& (unsigned char) s2[iterator] != '\0') \
-	&& iterator < n)
+	if (n == 0)
+		return (0);
+	while (s1[iterator] != '\0' && iterator < n - 1)
 	{
 		if (s1[iterator] != s2[iterator])
-			return ((s1[iterator] - s2[iterator]));
+			return (((unsigned char)s1[iterator] - \
+			(unsigned char)s2[iterator]));
 		++iterator;
 	}
-	return ((0));
+	return ((unsigned char)s1[iterator] - (unsigned char)s2[iterator]);
 }

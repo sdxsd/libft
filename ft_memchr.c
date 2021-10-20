53a1b7e1fb61b6c78f3dd224fd871812b94eb5ce
sdxsd <willmaguire@icloud.com>
2021-10-20 22:00:02 +0200

    ft_split() is finally done, I am so happy

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_memchr.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2021/10/13 10:24:18 by keizerrijk    #+#    #+#                 */
-/*   Updated: 2021/10/20 16:45:38 by wmaguire      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmaguire <wmaguire@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/13 10:24:18 by keizerrijk        #+#    #+#             */
+/*   Updated: 2021/10/20 21:56:06 by wmaguire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (iterator < n)
 	{
 		if ((unsigned char)str_ptr[iterator] == (unsigned char)c)
-			return ((void *) &str_ptr[iterator]);
+			return ((void *)&str_ptr[iterator]);
 		++iterator;
 	}
 	return (NULL);
--- ft_split.c
+++ ft_split.c
@@ -6,54 +6,40 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 13:22:31 by wmaguire          #+#    #+#             */
-/*   Updated: 2021/10/19 18:46:36 by wmaguire         ###   ########.fr       */
+/*   Updated: 2021/10/20 21:57:05 by wmaguire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 //#include <stdio.h>
 
-static int	ft_splitcount(const char *s, char c)
+static int	ft_wordcount(const char *s, char c)
 {
-	int	sc;
+	int	wc;
 
-	sc = 0;
-	while (*s != '\0')
+	if (!s)
+		return (-1);
+	wc = 0;
+	while (*s)
 	{
 		if (*s == c)
+			s++;
+		if (*s != c && *s != '\0')
 		{
-			sc++;
-			while (*s == c)
-			{
+			wc++;
+			while (*s != c && *s)
 				s++;
-				if (*s == '\0')
-				{
-					sc--;
-					return (sc);
-				}
-			}
 		}
-		s++;
 	}
-	return (sc);
+	return (wc);
 }
 
-static int	ft_getnextword(const char *s, char c)
+static int	ft_gndl(const char *s, char c)
 {
 	int	iterator;
 
 	iterator = 0;
-	while (s[iterator] == c)
-		++iterator;
-	return (iterator);
-}
-
-static int	ft_getnextdelim(const char *s, char c)
-{
-	int	iterator;
-
-	iterator = 0;
-	while (s[iterator] != c)
+	while (s[iterator] != c && s[iterator])
 		++iterator;
 	return (iterator);
 }
@@ -61,33 +47,47 @@ static int	ft_getnextdelim(const char *s, char c)
 char	**ft_split(char const *s, char c)
 {
 	char	**s_array;
-	int		iterator;
-	int		iterator_2;
-	int		next_delim;
+	size_t	iterator;
 
 	iterator = 0;
-	iterator_2 = 0;
 	if (!s)
 		return (NULL);
-	s_array = malloc(sizeof(char *) * (ft_splitcount(s, c) + 2));
+	s_array = malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
 	if (!s_array)
 		return (NULL);
-	while (iterator < ft_splitcount(s, c))
+	while (s_array && iterator < ft_strlen(s))
 	{
-		if (s[iterator_2] == c)
-			iterator_2 += ft_getnextword(&s[iterator_2], c);
-		next_delim = ft_getnextdelim(&s[iterator_2], c);
-		s_array[iterator] = ft_strndup(&s[iterator_2], next_delim);
-		iterator_2 += next_delim;
-		++iterator;
+		while (s[iterator] == c && s[iterator] != '\0')
+			iterator++;
+		if (s[iterator] != c && s[iterator] != '\0')
+		{
+			*s_array = ft_strndup(&s[iterator], ft_gndl(&s[iterator], c));
+			if (!*s_array)
+				return (NULL);
+			iterator += ft_gndl(&s[iterator], c);
+			s_array += 1;
+		}
 	}
-	return (s_array);
+	*s_array = NULL;
+	return (s_array - (ft_wordcount(s, c)));
 }
 
 /*
 int main()
 {
-	char	*s = "MacOS/Windows/GNU_LINUX/FreeBSD/OpenBSD/UNIX";
+	char	*s = "a//////////";
+	char	c = '/';
+	char	**s_array = ft_split(s, c);
+	
+	for (int i = 0; s_array[i] != NULL; i++)
+		printf("%s\n", s_array[i]);
+}
+*/
+
+/*
+int main()
+{
+	char	*s = "MacOS/Windows///GNU_LINUX/FreeBSD/////////OpenBSD/UNIX/";
 	char	c = '/';
 	char	**s_array = ft_split(s, c);
 	

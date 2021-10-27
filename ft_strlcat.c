69e02c6932484ab6a29b76a884914d25b9639e77
sdxsd <willmaguire@icloud.com>
2021-10-27 13:31:54 +0200

    Fixed strlcat(maybe)

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,11 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/25 20:12:08 by wmaguire      #+#    #+#                 */
-/*   Updated: 2021/10/27 13:08:22 by wmaguire      ########   odam.nl         */
+/*   Updated: 2021/10/27 13:31:23 by wmaguire      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -31,17 +30,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[iterator + offset] = src[iterator];
 		iterator++;
 	}
-	dst[iterator] = '\0';
+	dst[iterator + offset] = '\0';
 	return (offset + ft_strlen(src));
 }
 
+/*
 int main()
 {
-	char str[20] = "GNU + ";
+	char str[20] = "";
 	char *str2 = "LINUX";
 	size_t max = (ft_strlen(str) + ft_strlen(str2) + 1);
 
 	ft_strlcat(&str[0], str2, max);
 	max++;
-	printf("%s", &str[6]);
+	printf("%s", &str[0]);
 }
+*/

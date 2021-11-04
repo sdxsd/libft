0d7d049d74cef7393d0351ac67da885e785a1ebc
sdxsd <willmaguire@icloud.com>
2021-11-04 17:00:42 +0100

    fixed norm error

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/13 10:23:03 by keizerrijk    #+#    #+#                 */
-/*   Updated: 2021/11/04 11:28:55 by wmaguire      ########   odam.nl         */
+/*   Updated: 2021/11/04 16:49:29 by wmaguire      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 char	*ft_strchr(const char *s, int c)
 {
 	int		iterator;
-	char	*loc;
 
 	iterator = 0;
 	while (s[iterator] != (unsigned char)c && s[iterator] != '\0')
 		++iterator;
 	if (s[iterator] == (unsigned char)c)
-		return (loc = (char *) s + iterator);
+		return ((char *)s + iterator);
 	else
 		return (NULL);
 }

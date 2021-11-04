91534aa10027cb08207a4695921f7cf43d2a3fdc
sdxsd <willmaguire@icloud.com>
2021-11-04 17:04:53 +0100

    Another norm error fixed

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/19 12:03:35 by wmaguire      #+#    #+#                 */
-/*   Updated: 2021/11/04 13:59:57 by wmaguire      ########   odam.nl         */
+/*   Updated: 2021/11/04 17:04:24 by wmaguire      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (start >= ft_strlen(s))
-		return (substr = ft_calloc(1, 1));
+	{
+		substr = ft_calloc(1, 1);
+		return (substr);
+	}
 	if (len >= ft_strlen(s))
 		len = ft_strlen(s);
 	ptr = (char *)&s[start];
